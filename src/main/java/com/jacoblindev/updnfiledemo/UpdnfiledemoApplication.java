a0136dbbd487f99fc3d567def74cc36bfcfc7f5b
Jacob <jacoblindev@gmail.com>
2021-11-19 14:54:18 +0800

    chore: Initial commit after project initialized

---
+++ src/main/java/com/jacoblindev/updnfiledemo/UpdnfiledemoApplication.java
@@ -0,0 +1,13 @@
+package com.jacoblindev.updnfiledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpdnfiledemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpdnfiledemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jacoblindev/updnfiledemo/UpdnfiledemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacoblindev.updnfiledemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpdnfiledemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
